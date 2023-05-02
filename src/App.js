976e62d6bae7698b554dfa01c9ddff29a90dfefc
jackchealdang <jackchealdang@gmail.com>
2023-05-01 23:09:04 -0500

    create styling and App.js structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
 import {CTA, Brand, Navbar} from './components';
+import './App.css'
 
 const App = () => {
   return (
@@ -10,6 +11,13 @@ const App = () => {
         <Navbar />
         <Header />
       </div>
+      <Brand />
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
